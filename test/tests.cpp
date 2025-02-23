7991da19c703d11bf9e81b63b0052cd206e1a768
Artem Z <67142290+aortemon@users.noreply.github.com>
2025-02-23 23:14:36 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,13 +5,51 @@
 #include "alg.h"
 
 
+TEST(st1, checkPrime1) {
+  EXPECT_EQ(checkPrime(2000000), false);
+}
+TEST(st1, checkPrime2) {
+  EXPECT_EQ(checkPrime(1), true);
+}
+TEST(st1, checkPrime3) {
+  EXPECT_EQ(checkPrime(2), true);
+}
+TEST(st1, checkPrime4) {
+  EXPECT_EQ(checkPrime(3), true);
+}
+TEST(st1, checkPrime5) {
+  EXPECT_EQ(checkPrime(79), true);
+}
+TEST(st1, checkPrime6) {
+  EXPECT_EQ(checkPrime(3), false);
+}
+TEST(st1, nPrime1) {
+  EXPECT_EQ(nPrime(1), 1);
+}
+TEST(st1, nPrime2) {
+  EXPECT_EQ(nPrime(2), 2);
+}
+TEST(st1, nPrime3) {
+  EXPECT_EQ(nPrime(3), 3);
+}
+TEST(st1, nPrime4) {
+  EXPECT_EQ(nPrime(4), 5);
+}
+TEST(st1, nPrime5) {
+  EXPECT_EQ(nPrime(5), 7);
+}
+TEST(st1, nextPrime1) {
+  EXPECT_EQ(nextPrime(11), 13);
+}
+TEST(st1, nextPrime2) {
+  EXPECT_EQ(nextPrime(15), 17);
+}
+TEST(st1, nextPrime3) {
+  EXPECT_EQ(nextPrime(200), 211);
+}
 TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+  EXPECT_EQ(sumPrime(5), 18);
 }
 TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+  EXPECT_EQ(sumPrime(6), 29);
 }
